8d29b012b396ec1fe1423c2c66b0f2ac22e8256e
Felipe Leite <lfelipe.leite86@gmail.com>
2022-02-02 18:59:17 -0300

    Basic scene structure

---
+++ Assets/Scripts/Pause/Pause.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Pause : MonoBehaviour
+{
+    private bool IsPaused { get; set; }
+
+    [SerializeField] private GameObject m_PausePanel;
+
+    public void Show()
+    {
+        IsPaused = true;
+        m_PausePanel.SetActive(IsPaused);
+        Time.timeScale = 0.0f;
+    }
+
+    public void Hide()
+    {
+        m_PausePanel.SetActive(false);
+        IsPaused = false;
+        Time.timeScale = 1.0f;
+    }
+
+    public void Toggle()
+    {
+        if (IsPaused)
+            Hide();
+        else 
+            Show();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) Toggle();
+    }
+}
---
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    [Header("Transition Modes")]
+    [SerializeField] private bool _changeSceneByInput;
+    [SerializeField] private bool _changeSceneByTime;
+
+    [Header("Transition Parameters")]
+    [SerializeField] private float _timeToChange = 3.0f;
+    [SerializeField] private string _sceneName;
+    [SerializeField] private AudioClip _audioClip;
+    [SerializeField] private bool _useTimedLoading = false;
+
+    private bool _isLoading = false;
+
+    private void Start()
+    {
+        if (_changeSceneByTime)
+            Invoke(nameof(LoadLevel), _timeToChange);
+    }
+
+    private void Update()
+    {
+        if (!_changeSceneByInput)
+            return;
+
+        if (Input.anyKeyDown)
+            LoadLevel();
+    }
+
+    private void LoadLevel()
+    {
+        if (_isLoading)
+            return;
+
+        if (_audioClip)
+            AudioSource.PlayClipAtPoint(_audioClip, Camera.main.transform.position);
+
+        _isLoading = true;
+
+        if (_useTimedLoading)
+            ScreenManager.Instance.LoadLevelLoading(_sceneName);
+        else
+            ScreenManager.Instance.LoadLevel(_sceneName);
+    }
+}
